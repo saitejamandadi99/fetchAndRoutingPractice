6809452bfbddd3838dd64f1c0a3bd03204229aa7
saitejamandadi99 <saitejamandadi99>
2024-09-13 06:24:30 +0000

    before fetching

---
+++ src/components/Home/index.js
@@ -0,0 +1,12 @@
+import UserInfo from '../UserInfo'
+
+import './index.css'
+
+const Home = () => (
+  <div className="home-container">
+    Render UserInfo and BlogList
+    <UserInfo />
+  </div>
+)
+
+export default Home
